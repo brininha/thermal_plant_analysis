d0d266959fe91adfa62cfbeec66783ca600df83e
Sabrina Cristan <sabrina.cristan@gmail.com>
2026-08-22 20:31:24 -0300

    fix: add metadata details to adjust exhibition

--- app_completo.py
+++ app_completo.py
@@ -306,7 +306,7 @@ with tab_edit:
              st.subheader("Revisão de Segmentação (Correção Manual)")
              st.info("Caso a segmentação automática tenha falhado para alguma imagem, selecione-a abaixo para corrigir o recorte.")
              
-             opcoes_edicao = {f"{d['meta']['Planta']} ({d['meta']['Tratamento']})": i for i, d in enumerate(st.session_state['dados'])}
+             opcoes_edicao = {f"{d['meta']['Planta']} - {d['meta']['Tratamento']} - {d['meta']['Periodo']} - R{d['meta']['Replica']}": i for i, d in enumerate(st.session_state['dados'])}
              escolha_edicao = st.selectbox("Escolha a amostra para revisar/editar:", ["Selecione..."] + list(opcoes_edicao.keys()))
              
              if escolha_edicao != "Selecione...":
@@ -477,7 +477,7 @@ with tab_dash:
         st.markdown("### Inspeção de pixels")
         st.info("Selecione uma amostra para visualizar o mapa térmico radiométrico completo da área recortada. Passe o mouse sobre os pixels para ver a temperatura exata.")
         
-        opcoes = {f"{d['meta']['Planta']} ({d['meta']['Tratamento']})": i for i, d in enumerate(st.session_state['dados'])}
+        opcoes = {f"{d['meta']['Planta']} - {d['meta']['Tratamento']} - {d['meta']['Periodo']} - R{d['meta']['Replica']}": i for i, d in enumerate(st.session_state['dados'])}
         escolha = st.selectbox("Escolha a amostra para inspecionar:", list(opcoes.keys()))
         
         if escolha:
